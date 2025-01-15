06e6a54fa202bfe03fddde9fea26c53e007275a8
emf-99 <113733097+emf-99@users.noreply.github.com>
2025-01-14 20:06:27 -0500

    Initial Commit

---
+++ idm362-emf99/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idm362-emf99
+//
+//  Created by ella fromherz on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idm362-emf99/idm362_emf99App.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_emf99App.swift
+//  idm362-emf99
+//
+//  Created by ella fromherz on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_emf99App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
