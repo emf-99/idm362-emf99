1efdc2a36b3e6b42de465dedf9279abaedce0f05
emf-99 <113733097+emf-99@users.noreply.github.com>
2025-01-17 12:32:14 -0500

    hwk 01: text, headshot, and button that changes when clicked from my name (text) and headshot to my dog's name and photo.

--- idm362-emf99/ContentView.swift
+++ idm362-emf99/ContentView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScout = false
+
     var body: some View {
-        VStack {
-            Text("Hey, world!")
+        VStack(alignment: .center) {
+            Text(showingScout ? "Scout Fromherz" : "Ella Fromherz")
+                    .font(.title)
+                    .fontWeight(.thin)
+                    .padding(.all, 10)
+            
+            if showingScout {
+                CircleImageScout()
+                    .padding(.all, 10)
+            } else {
+                CircleImageHeadshot()
+                    .padding(.all, 10)
+
+            }
+
+            Button(action: {
+                showingScout.toggle()
+            }) {
+                Image(systemName: "pawprint.fill")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .frame(width: 50, height: 50)
+                    .background(Color.pink)
+                    .clipShape(Circle())
+                    .shadow(radius: 2)
+                    
+            }
+            .padding(.all, 10)
         }
-        .padding()
+        .padding(.all, 10)
     }
 }
+
 
 #Preview {
     ContentView()
